5e1aa1f9eb636c24dd1613aa5f6543fca6381871
Pedro Malo <pedromalo@live.com>
2025-03-11 19:50:23 +0100

    add: implement GET /movies endpoint to retrieve movie list and details

---
+++ nodejs/movies-api.js
@@ -0,0 +1,70 @@
+const http = require("http");
+
+const db = [
+  {
+    id: 1,
+    title: "La naranaja mecánica",
+    year: 1972,
+    duration: 120,
+    director: "Kubrick",
+    isSaga: false,
+  },
+  {
+    id: 2,
+    title: "El Apartamento",
+    year: 1964,
+    duration: 90,
+    director: "Billy Wilder",
+    isSaga: false,
+  },
+  {
+    id: 3,
+    title: "Harry Potter I",
+    year: 2002,
+    duration: 100,
+    director: "Chris Columbus",
+    isSaga: true,
+  },
+];
+
+const server = http.createServer(async (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  const [, endpoint, rawParam] = url.split("/");
+
+  if (endpoint === "movies" && !rawParam && method === "GET") {
+    res.writeHead(200);
+    res.end(JSON.stringify(db));
+    return;
+  }
+
+  if (endpoint === "movies" && rawParam && method === "GET") {
+    const param = parseInt(rawParam);
+
+    if (!param) {
+      res.writeHead(400);
+      res.end("Please provide a valid movie ID");
+      return;
+    }
+
+    const movie = db.find((movie) => movie.id === param);
+
+    if (!movie) {
+      res.writeHead(404);
+      res.end(`Movie with ID ${param} was not found`);
+      return;
+    }
+
+    res.writeHead(200);
+    res.end(JSON.stringify(movie));
+    return;
+  }
+
+  res.writeHead(404);
+  res.end("Method not found");
+});
+
+server.listen(3000, () => {
+  console.log("movies API is running at http://localhost:3000 🎬");
+});
